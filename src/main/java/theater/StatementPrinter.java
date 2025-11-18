c3f61a3aaffdfd29a61a290a3812df4b5c687927
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-11-18 15:59:29 -0500

    Task 2.4: Removing Total Volume Credits and Total Amount

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -22,29 +22,40 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
+
         final StringBuilder result = new StringBuilder("Statement for "
                 + invoice.getCustomer()
                 + System.lineSeparator());
-
-        for (Performance p : invoice.getPerformances()) {
-
-            // add volume credits
-            volumeCredits += getVolumeCredits(p);
-
+        for (Performance performance : invoice.getPerformances()) {
             // print line for this order
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
-                    getPlay(p).getName(),
-                    usd(getAmount(p)),
-                    p.getAudience()
+                    getPlay(performance).getName(),
+                    usd(getAmount(performance)),
+                    performance.getAudience()
             ));
-            totalAmount += getAmount(p);
         }
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getAmount(performance);
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            // add volume credits
+            result += getVolumeCredits(performance);
+        }
+        return result;
     }
 
     private static String usd(int totalAmount) {
